38bea588a6c435bfa31aef93f697f0a3926bf313
valentinaaranggo <valea28@gmail.com>
2023-04-08 20:54:20 -0400

    0.4.1
    fixed a post request bug that did not allow me to look at previous notes, used prettier formatting in the index.js file, and am currently fixing delete request route which only allows me to delete notes before I write a note

--- routes/api.js
+++ routes/api.js
@@ -4,12 +4,15 @@ const route =require('express').Router();
 const noteData= require('../db/db.json');
 const fs= require('fs');
 const userId= uuidv4()
+const path= require('path');
+const { send } = require('process');
 
 
 
 console.log( "our json db has this: ", noteData)
 
 
+//get request to serve db in json
 route.get('/notes', (req, res) =>{
 
     fs.readFile('./db/db.json', 'utf8',(err,data) => {
@@ -24,67 +27,57 @@ route.get('/notes', (req, res) =>{
 
 });
 
+//post request for a new note
 route.post('/notes',(req,res) => {
 const {title, text} = req.body;
+var id= userId;
 const newNote= {
     title,
     text,
-    notes_id: userId
-};{
-if (title && text){
+    id
+};
     fs.readFile('./db/db.json', 'utf8',(err,data) => {
         if (err){
             console.error(err);
         } else {
             const parsedData= JSON.parse(data)
             parsedData.push(newNote)
-          fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), writeErr => {
-            writeErr? console.error(writeErr):console.info("note written to file system");
+          fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 5), writeErr => {
+            writeErr? console.error(writeErr): console.log("note written to file system");
 
           }
-          )
+          );
+          res.send()
         }
 
     }) 
-    const response={
-        message: "success",
-        body: newNote
-    }
-    console.log(response)
-    res.status(201).json(response)
-
-    }else{
-        res.status(500).json('error in posting review');
-    }
-    
-}
-
-
-});
-
-route.delete('/notes/:id',(req,res) => {
-    const requestedId= req.params.id;
-    fs.readFile('./db/db.json','utf8', (err, data)=>{
-        if (err){
-            console.log('error reading file');
-        } else{
-            const parsedData= JSON.parse(data)
-            const delNoteIndex= parsedData.findIndex((note) => note.id == requestedId);
-                parsedData.splice(delNoteIndex,1);
-                fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 5), (writeErr) => {
-                if (writeErr) {
-                console.log("error writing file")
-                 } else{ 
-                     console.log("note has been deleted");
-                     res.send()
-                 }
-        });
-       
-    }
-})
-
-
+  
     });
+ 
+//delete request to delete a note
+route.delete('/notes/:id',(req,res) => {
+    const requestedId=req.params.id
+   for (var i=0; i< noteData.length; i++){
+    var note=noteData[i];
+    
+    if (note.id=== requestedId){
+        noteData.splice(i,1);
+        fs.writeFile('./db/db.json', JSON.stringify(noteData, null, 5) , err => {
+            if (err){
+             console.log(err)
+             }else{
+                console.log("object deleted");
+                res.send();
+             } 
+            
+        });
+    }
+   }
+           
+    
+       
+    });
+
 
 
     
--- server.js
+++ server.js
@@ -1,21 +1,27 @@
-const express= require('express');
-const path= require('path');
-const apiRouter= require('./routes/api');
+const express = require("express");
+const path = require("path");
+const apiRouter = require("./routes/api");
 
-const app= express();
-const PORT= process.env.port || 3001; //base route
+const app = express();
+const PORT = process.env.port || 3001; //base route
 
-app.use(express.static('public')); // middleware to serve static files in public folder
+app.use(express.static("public")); // middleware to serve static files in public folder
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+); // serves index.html file to / route
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'))); // serves index.html file to / route
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+); // serves notes.html file to /notes route
 
-app.get('/notes',(req, res) =>  res.sendFile(path.join(__dirname,'public/notes.html')));// serves notes.html file to /notes route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
-
-app.listen(PORT,()=>
+app.listen(PORT, () =>
   console.log(`app is listening at http://localhost:${PORT}`)
-  );
+);
