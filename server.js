2f070db3c7fa20715ccc69c84ee5ee61e54453ba
valentinaaranggo <valea28@gmail.com>
2023-04-08 22:29:40 -0400

    0.4.3
    fixed deployment bug

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const apiRouter = require("./routes/api");
 
 const app = express();
-const PORT = process.env.port || 3001; //base route
+const PORT = process.env.PORT || 3001; //base route
 
 app.use(express.static("public")); // middleware to serve static files in public folder
 app.use(express.json());
